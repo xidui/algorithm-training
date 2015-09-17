9d32db2895c217a13908b5ead18f8f0aa4120e17
Jerry Zou <zry656565@gmail.com>
2015-09-17 23:08:37 +0800

    Another Solution of #1 Two Sum

--- leetcode/Two Sum/solution.js
+++ leetcode/Two Sum/solution.js
@@ -59,3 +59,27 @@ var twoSum = function(nums, target) {
     }
     return result;
 };
+
+/****
+ * @author: Jerry Zou
+ * @date: 2015.6.4
+ **/
+
+// Runtime: 140ms
+
+var twoSum = function(nums, target) {
+    var i, j, num, hash = {};
+
+    for (i = 0; i < nums.length; i++) {
+        hash[nums[i]] = i + 1;
+    }
+    for (i = 0; i < nums.length; i++) {
+        j = hash[target - nums[i]];
+        if (j && j !== i + 1) {
+            i = i + 1;
+            return i < j ? [i, j] : [j, i];
+        }
+    }
+    return [-1, -1];
+    //
+};
