1b4343d1e8b92967392dfdd1bc1fbd47a7c1e424
xidui <664984593@qq.com>
2015-10-29 19:34:20 +0800

    #34 another c++

--- leetcode/Search for a Range/solution.cpp
+++ leetcode/Search for a Range/solution.cpp
@@ -45,3 +45,34 @@ public:
         return ret;
     }
 };
+
+// shorter solution
+// it is smarter
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& A, int target) {
+        int n = A.size();
+        int i = 0, j = n - 1;
+        vector<int> ret(2, -1);
+        // Search for the left one
+        while (i < j)
+        {
+            int mid = (i + j) /2;
+            if (A[mid] < target) i = mid + 1;
+            else j = mid;
+        }
+        if (A[i]!=target) return ret;
+        else ret[0] = i;
+
+        // Search for the right one
+        j = n - 1;  // We don't have to set i to 0 the second time.
+        while (i < j)
+        {
+            int mid = (i + j) /2 + 1;   // Make mid biased to the right
+            if (A[mid] > target) j = mid - 1;
+            else i = mid;               // So that this won't make the search range stuck.
+        }
+        ret[1] = j;
+        return ret;
+    }
+};
