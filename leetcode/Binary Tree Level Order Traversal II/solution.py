36e557caeb8f0d76bed7ef34cd5375a783f66d68
xidui <664984593@qq.com>
2015-11-10 19:49:39 +0800

    #107 python solution

---
+++ leetcode/Binary Tree Level Order Traversal II/solution.py
@@ -0,0 +1,15 @@
+// 60ms
+// almost the same solution with #102
+class Solution(object):
+    def levelOrderBottom(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        ans, level = [], [root]
+        while root and level:
+            temp = ans
+            ans = [[node.val for node in level]]
+            ans.extend(temp)
+            level = [kid for n in level for kid in (n.left, n.right) if kid]
+        return ans
