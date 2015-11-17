5923e4bacaa93151334e40d0bab735f7791261f1
Jerry Zou <jerry.zry@outlook.com>
2015-11-17 14:20:07 +0800

    fixup! [JS] #14 Longest Common Prefix

--- leetcode/Longest Common Prefix/solution.js
+++ leetcode/Longest Common Prefix/solution.js
@@ -1,22 +1,31 @@
+/**********
+ * Solution of Longest Common Prefix
+ * https://leetcode.com/problems/longest-common-prefix/
+ * @author: Jerry Zou
+ * @email: jerry.zry@outlook.com
+ * 
+ * Runtime: 148ms
+ * Beats 13.79% JS submissions
+ */
+
 /**
  * @param {string[]} strs
  * @return {string}
  */
 var longestCommonPrefix = function(strs) {
-	if (strs.length == 0) return ""
-	if (strs.length == 1) return strs[1]
+  if (strs.length == 0) return ""
+  if (strs.length == 1) return strs[0]
+  
+  strs.sort()
 	
-  var len = strs.length
-    , i = 1
-    , j = 0
-    , current
-
-  while (j < len) {
-    current = strs[0][j]
-    while (i < len) {
-      if (current != strs[i][j]) return strs[0].substr(0, j)
-    }
-    j++
-    i = 1
+  var first = strs[0]
+    , last = strs[strs.length - 1]
+    , i = 0
+    , len = first.length
+  
+  for (; i < len; ++i) {
+    if (first[i] != last[i]) return strs[0].substr(0, i)
   }
+  
+  return strs[0]
 }
