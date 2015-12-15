9dd27decccecd04248922690b2cb129638cec5e7
Jerry Zou <jerry.zry@outlook.com>
2015-12-15 21:15:17 +0800

    [JS] #28 Implement strStr

---
+++ leetcode/Implement strStr/solution.js
@@ -0,0 +1,15 @@
+/**********
+ * Implement strStr()
+ * https://leetcode.com/problems/implement-strstr/
+ * @author: Jerry Zou
+ * @email: jerry.zry@outlook.com
+ */
+
+/**
+ * @param {string} haystack
+ * @param {string} needle
+ * @return {number}
+ */
+var strStr = function(haystack, needle) {
+    return haystack.search(needle)
+}
