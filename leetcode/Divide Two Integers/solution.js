cde460c8a28d47d3a0245f52e6440f8ef5a8efd1
Jerry Zou <jerry.zry@outlook.com>
2015-12-19 00:14:01 +0800

    [JS] #29 another solution

--- leetcode/Divide Two Integers/solution.js
+++ leetcode/Divide Two Integers/solution.js
@@ -38,3 +38,63 @@ var divide = function(dividend, divisor) {
         return (flag1^flag2)?-result:result;
     }
 };
+
+
+/**********
+ * Anothor Solution to `Divide Two Integers`
+ * https://leetcode.com/problems/divide-two-integers/
+ * @author: Jerry Zou
+ * @email: jerry.zry@outlook.com
+ * 
+ * Runtime: 228ms | 2015.12.19 00:10 GMT+08
+ * Beats 80.00% JS submissions
+ */
+
+var divide = function(dividend, divisor) {
+  'use strict'
+	
+	const INT_MAX = 2147483647
+		, INT_MIN = -2147483648
+	
+	if (divisor === 0) return 0
+	if (dividend > INT_MAX ||
+			dividend < INT_MIN ||
+			(dividend == INT_MIN && divisor == -1)) return INT_MAX		
+	if (divisor === 1) return dividend
+	if (divisor === -1) return -dividend
+	
+	let times = 1
+		, result = 0
+		, isPositive = (dividend > 0 && divisor > 0) || (dividend < 0 && divisor < 0)
+	
+	dividend = Math.abs(dividend)
+	divisor = Math.abs(divisor)
+	
+	if (dividend < divisor) return 0
+	
+	while (dividend >= divisor) {
+		if (divisor >= 1073741824) {
+			result += times
+			dividend -= divisor
+		} else {
+		  divisor = divisor << 1
+			times = times << 1
+		}
+	}
+	
+	while (dividend < divisor) {
+		divisor = divisor >> 1
+		times = times >> 1
+		if (dividend === divisor) {
+			result += times
+			break
+		} else if (dividend > divisor) {
+			dividend -= divisor
+			result += times
+		}
+		if (times === 1) break
+	}
+	
+	result = isPositive ? result : -result
+	return result
+}
