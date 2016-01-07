011cb5555f05896761fd942f845e4e06d4d44ec2
Jerry Zou <jerry.zry@outlook.com>
2016-01-07 10:17:48 +0800

    crawl description of every problem on Leetcode

--- crawler/leetcode.js
+++ crawler/leetcode.js
@@ -1,10 +1,13 @@
 var Crawler = require("crawler");
-var url = require('url');
+var problems = {};
 
 var c = new Crawler({
-  maxConnections : 10,
-  // This will be called for each crawled page
-  callback : function (error, result, $) {
+  maxConnections : 10
+});
+
+c.queue({
+  uri: 'https://leetcode.com/problemset/algorithms/',
+  callback: function (error, result, $) {
     $('#problemList').find('tr').each(function(index, problem) {
       if ($(problem).find('.fa-lock').length > 0) return;
       var id = $(problem).find('td').eq(1).text()
@@ -12,9 +15,26 @@ var c = new Crawler({
         , link = $(problem).find('td').eq(2).find('a').attr('href')
         , ac = $(problem).find('td').eq(3).text()
         , difficulty = $(problem).find('td').eq(4).text();
-      console.log(id, name, link, ac, difficulty);
-    })
+      problems[id] = {
+        id: id,
+        name: name,
+        link: link,
+        ac: ac,
+        difficulty: difficulty
+      };
+
+      c.queue({
+        uri: 'https://leetcode.com' + link,
+        callback: function (error, result, $) {
+          var description = '';
+          $('.question-content>p').each(function(index, elem) {
+            var content = $(elem).html();
+            if (content) description += '<p>' + content + '</p>';
+          });
+          problems[id]['description'] = description;
+          console.log(problems[id]['description']);
+        }
+      });
+    });
   }
 });
-
-c.queue('https://leetcode.com/problemset/algorithms/');
