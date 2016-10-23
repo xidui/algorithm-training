171bd523138943576402cab548c53cc351354ec6
blueAnana <915935284@qq.com>
2016-10-23 23:55:16 +0800

    add annotate

--- leetcode/Find All Anagrams in a String/solution.py
+++ leetcode/Find All Anagrams in a String/solution.py
@@ -7,7 +7,7 @@ class Solution(object):
         """
         list = []
         dic = {}
-        
+        $
         for c in p:
             if dic.has_key(c):
                 dic[c] += 1
@@ -17,11 +17,12 @@ class Solution(object):
         numToBeZero = len(dic)
         
         for index in range(len(s)):
+			$ push on right
             if dic.has_key(s[index]):
                 dic[s[index]] -= 1
                 if dic[s[index]] == 0:
                     numToBeZero -= 1
-                    
+            $ push on left        
             if index >= len(p) and dic.has_key(s[index - len(p)]):
                 if dic[s[index - len(p)]] == 0:
                     numToBeZero += 1
